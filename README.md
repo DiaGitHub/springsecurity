c244083b0b6fabfc0b53cbacd9b08cd0b185aa64
Lyd <saighilydia@gmail.com>
2022-10-22 14:01:07 +0200

    [CHORE] JDBC user store

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# springsecurity
+# JDBC-based user store
+User information is often maintained in a relational database
+JDBC-based user store seems appropriate.
---
+++ src/main/java/fr/dyl/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package fr.dyl.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        var user = User.withDefaultPasswordEncoder()
+                               .username("user")
+                               .password("password")
+                               .roles("USER")
+                               .build();
+        var users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        return users;
+    }
+}
